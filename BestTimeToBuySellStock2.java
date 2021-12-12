64e85c37541bfd787f1c908726db448906d97df3
Jatin P M <jatinpm96@gmail.com>
2021-12-12 11:28:29 +0530

    Linting

--- BestTimeToBuySellStock2.java
+++ BestTimeToBuySellStock2.java
@@ -2,10 +2,10 @@ public class BestTimeToBuySellStock2 {
     // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
     class Solution {
         public int maxProfit(int[] prices) {
-            int profit =  0;
-            for(int i=1;i<prices.length;i++) {
-                if(prices[i]>prices[i-1])
-                    profit+=prices[i]-prices[i-1];
+            int profit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i] > prices[i - 1])
+                    profit += prices[i] - prices[i - 1];
             }
             return profit;
         }
